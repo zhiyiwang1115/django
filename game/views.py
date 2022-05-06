e2659fc257e02d79f6edabb1e311920fb9f3f7b2
zhiyi wang <zwan9517@uni.sydney.edu.au>
2022-05-06 16:41:41 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -2,3 +2,7 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1 style="text-align: center">Hello Word</h1>')
+
+def play(request):
+    return HttpResponse('<h1 style="text-align: center">Play Game</h1>')
+
